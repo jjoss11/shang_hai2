c6357519008a0dd4fa8dba86c587892f75f41842
Jacob <jacobjoss12@gmail.com>
2019-05-15 22:20:10 -0700

    new class to describe a set of cards with the same value

---
+++ Set.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Jacob on 5/15/2019.
+//
+
+#include "Set.h"
+#include <iostream>
+Set::Set(){
+    rank = -1;
+}
+void Set::to_string() {
+    int i = 1;
+    for(Card* c : cards){
+        std::cout << "[" << i << "] ";
+        c->to_string();
+        i++;
+    }
+}
+bool Set::add_card(Card *c) {
+    if(rank != -1 && c->value == rank)
+        cards.push_back(c);
+    else if(rank == -1) {
+        cards.push_back(c);
+        rank = c->value;
+    }
+    else {
+        std::cout << "ERROR in adding this card to the set, the value of the card does not match that of the rest of the cards in the set\n";
+        return false;
+    }
+    return true;
+}
---
+++ Set.h
@@ -0,0 +1,22 @@
+//
+// Created by Jacob on 5/15/2019.
+//
+
+#ifndef SHANG_HAI2_SET_H
+#define SHANG_HAI2_SET_H
+
+#include <vector>
+#include <iostream>
+#include "Card.h"
+class Set {
+public:
+    int rank;
+    std::vector<Card*> cards;
+
+    Set();
+    void to_string();
+    bool add_card(Card* c);
+};
+
+
+#endif //SHANG_HAI2_SET_H
